6c16129b573bb7b0c86930a78ff3ad1ab4c244c8
Bastien <bastien.arnaud@gmail.com>
2023-12-05 22:54:15 +0100

    Step 2 day 1 Done

--- src/aoc_2023/day1/day1_main.rs
+++ src/aoc_2023/day1/day1_main.rs
@@ -1,14 +1,14 @@
 use std::fs::read_to_string;
 use std::io;
 
-const FILE_PATH: &str = "./inputs/aoc_2023/day1/inputs_step1.txt";
+const FILE_PATH: &str = "./inputs/aoc_2023/day1/inputs_step2.txt";
 
 pub fn day1_main() -> io::Result<u32>{
     let mut sum = 0;
 
     for line in read_to_string(FILE_PATH)?.lines() {
         let mut line_value = 0;
-        let mut line_numbers = enigme1(line);
+        let mut line_numbers = enigme2(line);
 
         if line_numbers.len() == 1 {line_numbers.push(line_numbers[0]);}
 
@@ -24,4 +24,23 @@ pub fn day1_main() -> io::Result<u32>{
 
 pub fn enigme1(_line:&str) -> Vec<u32>{
     _line.chars().filter_map(|char| char.to_digit(10)).collect()
+}
+
+pub fn enigme2(_line:&str) -> Vec<u32> {
+    let mut line_numbers = vec![];
+    let words = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut indx = 0;
+
+    while indx < _line.len() {
+        for (idx_word, word) in words.iter().enumerate() {
+            if _line[indx..].starts_with(word) {
+                line_numbers.push((&idx_word%10) as u32);
+                break;
+            }
+        }
+
+        indx += 1;
+    }
+
+    line_numbers
 }
