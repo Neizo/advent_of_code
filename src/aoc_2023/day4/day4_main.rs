e7c8a38fa677938aad6e165e179f75178245eacb
Bastien <bastien.arnaud@gmail.com>
2023-12-10 20:03:00 +0100

    Day4 step 2 done

--- src/aoc_2023/day4/day4_main.rs
+++ src/aoc_2023/day4/day4_main.rs
@@ -1,20 +1,30 @@
 use std::fs::read_to_string;
 use std::io;
 use io::Result;
+use std::ops::Range;
 
 const FILE_PATH: &str = "./inputs/aoc_2023/day4/inputs.txt";
 
-pub fn day4_main() -> Result<u32> {
+pub fn day4_main() -> Result<(u32, u32)> {
     let mut winning_numbers_found = vec![];
+    let file = read_to_string(FILE_PATH)?;
+    let mut card_count = vec![0; file.lines().count()];
 
-    for line in read_to_string(FILE_PATH)?.lines() {
+    for (indx, line) in file.lines().enumerate() {
+        card_count[indx] += 1;
+
         let (winning_numbers, numbers) = parse_line(line)?;
         winning_numbers_found.push(winning_numbers.iter().filter_map(|winning_number| numbers.iter().position(|number| number == winning_number)).collect::<Vec<usize>>());
+
+        let range_indx = Range {start: 1, end: winning_numbers_found.last().unwrap().len()+1};
+        for index in range_indx {
+            card_count[indx+index] += 1 * card_count[indx];
+        }
     }
 
     winning_numbers_found.retain(|element| element.len() > 0);
 
-    Ok(winning_numbers_found.iter().map(|array| 2u32.pow((array.len()-1) as u32)).sum())
+    Ok((winning_numbers_found.iter().map(|array| 2u32.pow((array.len()-1) as u32)).sum(), card_count.iter().sum::<u32>()))
 }
 
 fn parse_line(line:&str) -> Result<(Vec<u32>, Vec<u32>)>{
