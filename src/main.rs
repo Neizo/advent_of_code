1dc91032814b24246ab90a43f2c9c5e51eccedf6
Bastien <bastien.arnaud@gmail.com>
2023-12-10 11:47:05 +0100

    Day4 step 1 done

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod aoc_2023;
 use crate::aoc_2023::day1::day1_main::day1_main;
 use crate::aoc_2023::day2::day2_main::day2_main;
 use crate::aoc_2023::day3::day3_main::day3_main;
+use crate::aoc_2023::day4::day4_main::day4_main;
 
 fn main() {
     println!("Day 1 : The answer is {:?}", day1_main().unwrap());
     println!("Day 2 : The answer is {:?}", day2_main().unwrap());
     println!("Day 3 : The answer is {:?}", day3_main().unwrap());
+    println!("Day 4 : The answer is {:?}", day4_main().unwrap());
 }
 
 /*
