301d951f4cb6565fd33864832c56c3f2ff27e6e8
Bastien <bastien.arnaud@gmail.com>
2023-12-13 10:18:06 +0100

    Day6 step 1 & 2 done

--- src/aoc_2023/day6.rs
+++ src/aoc_2023/day6.rs
@@ -14,9 +14,7 @@ struct Races {
 pub fn day6_main() -> Result<(u64, u64)>{
     let races = parse_file(read_to_string(FILE_PATH)?.lines());
 
-    println!("{:?}", races);
-
-    Ok((42, 42))
+    Ok((part_1(&races), part_2(&races)))
 }
 
 fn parse_file(lines:Lines) -> Vec<Races>{
@@ -38,4 +36,27 @@ fn extract_value(line: &str) -> Vec<&str> {
     let str_value: Vec<&str> = line.split(":").last().unwrap().trim().split_whitespace().collect();
 
     str_value
+}
+
+fn part_1(races:&Vec<Races>) -> u64 {
+    let simulation: Vec<u64> = races.iter().
+        map(|race| (0..=race.time).map(|time_holding| (race.time-time_holding) * time_holding).collect::<Vec<u64>>()
+            .iter().fold(0u64, |nb_winning, distance_simulation| {
+            if distance_simulation > &race.distance {nb_winning + 1} else {nb_winning}
+        })
+        ).collect::<Vec<_>>();
+
+    simulation.iter().fold(1, |value, simu| {value * simu})
+}
+
+fn part_2(races:&Vec<Races>) -> u64 {
+    let mut races_e2=vec![];
+
+    let (time, distance) = races.iter().rev().enumerate().fold((0, 0), |(time, distance), (_, race) | {
+        (time + (race.time * 10u64.pow(time.to_string().chars().count() as u32)), distance + (race.distance * 10u64.pow(distance.to_string().chars().count() as u32)))
+    });
+
+    races_e2.push(Races{time: time/10, distance: distance/10});
+
+    part_1(&races_e2)
 }
--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,5 @@ fn main() {
     println!("Day 3 : The answer is {:?}", day3_main().unwrap());
     println!("Day 4 : The answer is {:?}", day4_main().unwrap());
     println!("Day 5 : The answer is {:?}", day5_main().unwrap());
-    println!("Day 5 : The answer is {:?}", day6_main().unwrap());
+    println!("Day 6 : The answer is {:?}", day6_main().unwrap());
 }
