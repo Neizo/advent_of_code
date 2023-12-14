7e7d148bb0fcce5e65fa618b2edcabdadcfdc41b
Bastien <bastien.arnaud@gmail.com>
2023-12-14 21:21:04 +0100

    Day9 step 1 & 2 done

--- src/aoc_2023/day9.rs
+++ src/aoc_2023/day9.rs
@@ -6,37 +6,40 @@ use num::Signed;
 
 const FILE_PATH: &str = "./inputs/aoc_2023/day9/inputs.txt";
 
-pub fn day9_main() -> Result<(i64, u64)>{
+pub fn day9_main() -> Result<(i64, i64)>{
     let numbers = read_to_string(FILE_PATH)?.lines().map(|line| extract_number(line)).collect::<Vec<Vec<i64>>>();
 
-
-
-    Ok((part_1(&numbers),42))
+    Ok((part_1(&numbers), part_2(&numbers)))
 }
 
 fn extract_number(line:&str) -> Vec<i64> {
    line.trim().split_whitespace().map(|value_str| value_str.parse::<i64>().unwrap()).collect::<Vec<i64>>()
 }
 
-fn part_1(numbers:&Vec<Vec<i64>>) -> i64 {
+fn part_1(numbers:&Vec<Vec<i64>>) -> i64{
     numbers.iter().map(|vec_element| vec_element.last().unwrap() + extract_diff(&vec_element)).collect::<Vec<i64>>().iter().sum()
 }
 
 fn extract_diff(numbers:&Vec<i64>) -> i64{
     let mut diffs = vec![];
 
-    println!("{:?}", numbers);
+    diffs.push(numbers[0]);
+    diffs = (1..numbers.len()).map(|indx| numbers[indx] - numbers[indx-1]).collect::<Vec<i64>>();
 
-    for indx in 0..numbers.len()-1 {
-        diffs.push(numbers[indx+1] - numbers[indx]);
-    }
-
-    if diffs.iter().sum::<i64>() != 0 {
+    if diffs.iter().any(|&diff| diff != 0) == true {
         return diffs.last().unwrap() + extract_diff(&diffs);
     }
 
     return 0i64
 }
 
-//1984151189
-//1974913982
+fn part_2(numbers:&Vec<Vec<i64>>) -> i64 {
+    let mut sum = 0;
+
+    for mut number in numbers.clone() {
+        number.reverse();
+        sum += number.last().unwrap() + extract_diff(&number);
+    }
+
+    sum
+}
