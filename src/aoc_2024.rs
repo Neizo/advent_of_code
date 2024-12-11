80f1f4a4d6e6acceafe96d4bc31f56c9ff39b2c3
Bastien <bastien.arnaud@gmail.com>
2024-12-11 08:14:13 +0100

    day11

--- src/aoc_2024.rs
+++ src/aoc_2024.rs
@@ -8,9 +8,10 @@ mod day7;
 mod day9;
 mod day8;
 mod day10;
+mod day11;
 
 pub fn resout_aoc_2024() {
-    println!("Reponse jour 1 : {:?}", day1::get_response());
+    /*println!("Reponse jour 1 : {:?}", day1::get_response());
     println!("Reponse jour 2 : {:?}", day2::get_response());
     println!("Reponse jour 3 : {:?}", day3::get_response());
     println!("Reponse jour 4 : {:?}", day4::get_response());
@@ -19,6 +20,7 @@ pub fn resout_aoc_2024() {
     println!("Reponse jour 7 : {:?}", day7::get_response());
     println!("Reponse jour 8 : {:?}", day8::get_response());
     println!("Reponse jour 9 : {:?}", day9::get_response());
-    println!("Reponse jour 10 : {:?}", day10::get_response());
+    println!("Reponse jour 10 : {:?}", day10::get_response());*/
+    println!("Reponse jour 11 : {:?}", day11::get_response());
 
 }
---
+++ src/aoc_2024/day11.rs
@@ -0,0 +1,47 @@
+use std::collections::{HashMap, VecDeque};
+use std::error::Error;
+use std::fs::read_to_string;
+
+const FILE_PATH: &str = "./inputs/aoc_2024/inputs_day11.txt";
+
+pub fn get_response() -> Result<(usize, usize), Box<dyn Error>> {
+    let inputs = parse_file();
+
+    Ok((enigme1(&inputs, 25), enigme1(&inputs, 75)))
+}
+
+pub fn parse_file() -> HashMap<u128, usize> {
+    read_to_string(FILE_PATH).expect("Can't open file").split_whitespace().map(|value| (value.parse::<u128>().unwrap(), 1) ).collect::<HashMap<u128, usize>>()
+}
+
+// Compte les pierres après un certain nombre de clignements
+fn enigme1(initial_stones: &HashMap<u128, usize>, blinks: usize) -> usize {
+    let mut stones = initial_stones.clone();
+
+    // Simulation des clignements
+    for _ in 0..blinks {
+        stones = update(&stones);
+    }
+
+    stones.values().sum()
+}
+
+fn update(old_stones: &HashMap<u128, usize>) -> HashMap<u128, usize> {
+    let mut stones = HashMap::with_capacity(old_stones.len());
+    for (&stone, &count_stone) in old_stones {
+        match stone {
+            0 => *stones.entry(1).or_default() += count_stone,
+            _ => {
+                let digits = stone.ilog10() + 1;
+                if digits % 2 == 0 {
+                    *stones.entry(stone % 10u128.pow(digits / 2)).or_default() += count_stone;
+                    *stones.entry(stone / 10u128.pow(digits / 2)).or_default() += count_stone;
+                } else {
+                    *stones.entry(stone * 2024).or_default() += count_stone
+                }
+            }
+        }
+    }
+
+    stones
+}
